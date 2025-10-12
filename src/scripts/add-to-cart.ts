8255d14d8d54a0abf058bd01deb4ea0eb015609a
paredes09 <111245800+paredes09@users.noreply.github.com>
2025-10-12 16:57:55 -0500

    add button addtocart prueba

---
+++ src/scripts/add-to-cart.ts
@@ -0,0 +1,28 @@
+// src/scripts/add-to-cart.ts
+import { postAddToCart } from "../lib/wp.ts";
+
+export function setupAddToCart(buttonSelector: string) {
+  const button = document.querySelector<HTMLButtonElement>(buttonSelector);
+  if (!button) return;
+
+  button.addEventListener("click", async () => {
+    const productId = button.dataset.id;
+    if (!productId) return;
+
+    button.disabled = true;
+    button.textContent = "Agregando...";
+
+    try {
+      await postAddToCart(Number(productId));
+      button.textContent = "✅ Agregado!";
+    } catch (err) {
+      console.error(err);
+      button.textContent = "❌ Error";
+    } finally {
+      setTimeout(() => {
+        button.textContent = "🛒 Agregar al carrito";
+        button.disabled = false;
+      }, 2000);
+    }
+  });
+}
