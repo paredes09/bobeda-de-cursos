bc039166760c3c11b430499a0d3fb025bcc546f8
paredes09 <111245800+paredes09@users.noreply.github.com>
2025-10-25 11:49:09 -0500

    prueba api 5

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -116,7 +116,7 @@ export const postAddToCart = async (productId : number) => {
   
   
   export const getCart = async () => {
-    const response = await fetch('https://vip.bovedadecursos2025.com/wp-json/wc/store/cart');
+    const response = await fetch('https://vip.bovedadecursos2025.com/wp-json/wc/store/v1/cart');
   
     if (!response.ok) throw new Error('Failed to fetch cart');
   
